822ecf382817b6f2ff140fd865d0434cb2349723
Kaciras <kaciras@outlook.com>
2019-02-05 22:12:18 +0800

    fuck git lost my changes

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Test")]
 namespace Core
 {
-	class Program
+	[Verb("download", HelpText = "下载图册")]
+	sealed class DownloadOptions
+	{
+		[Value(0, Required =true, HelpText ="图册网址")]
+		public string Uri { get; set; }
+	}
+
+	[Verb("statistics", HelpText = "启动统计爬虫")]
+	sealed class StatisticsOptions
+	{
+
+	}
+
+	static class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Parser.Default.ParseArguments<DownloadOptions, StatisticsOptions>(args)
+				.WithParsed<DownloadOptions>(DownloadGallery)
+				.WithParsed<StatisticsOptions>(RunStatisticsCrawler);
+		}
+
+		static void DownloadGallery(DownloadOptions options)
+		{
+			Console.WriteLine(options.Uri);
+		}
+
+		static void RunStatisticsCrawler(StatisticsOptions options)
+		{
+
 		}
 	}
 }
