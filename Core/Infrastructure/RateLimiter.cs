ade87ffe5d07803ccf1ea02e7433279c2603b27e
Kaciras <kaciras@outlook.com>
2019-03-09 20:15:54 +0800

    add RateLimiter

---
+++ Core/Infrastructure/RateLimiter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Core.Infrastructure
+{
+	public class RateLimiter
+	{
+		private readonly int maxPermits;
+		private readonly double stableInterval;
+
+		private int stored;
+		private DateTime lastAcquire;
+
+		public RateLimiter(int permits, TimeSpan timeSpan)
+		{
+			stored = maxPermits = permits;
+			stableInterval = timeSpan.TotalMilliseconds / maxPermits;
+		}
+
+		public bool TryAcquireFailed(int permits, out TimeSpan wait)
+		{
+			var mills = Acquire(permits);
+			if (mills > 0)
+			{
+				wait = TimeSpan.FromMilliseconds(mills);
+				return true;
+			}
+			else
+			{
+				wait = TimeSpan.Zero;
+				return false;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public double Acquire(int permits)
+		{
+			var now = DateTime.Now;
+
+			if (permits <= stored)
+			{
+				lastAcquire = now;
+				stored -= permits;
+				return 0;
+			}
+
+			// 需要的令牌数比上限还大，这个请求永远无法完成，属于异常情况
+			if (permits > maxPermits)
+			{
+				throw new ArgumentException("请求的令牌数量大于令牌桶的上限");
+			}
+
+			// 上次拿走令牌之后又过了多久
+			var restoreTime = (now - lastAcquire).TotalMilliseconds;
+
+			// 装到需要的数量需要的时间 = 还差的令牌数 * 每令牌所需时间
+			var wait = (permits - stored) * stableInterval;
+
+			if (wait <= restoreTime)
+			{
+				var restore = (int)((restoreTime - wait) * stableInterval);
+				lastAcquire = now;
+				stored = Math.Min(stored + restore, maxPermits);
+				return 0;
+			}
+
+			return wait - restoreTime;
+		}
+	}
+}
---
+++ Test/RateLimiterTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Core.Infrastructure;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+	[TestClass]
+	public class RateLimiterTest
+	{
+		[TestMethod]
+		public void AcquireSuccess()
+		{
+			var limiter = new RateLimiter(10, TimeSpan.FromSeconds(1));
+
+			limiter.Acquire(9).Should().BeLessOrEqualTo(0);
+		}
+
+		[TestMethod]
+		public void AcquireFailed()
+		{
+			var limiter = new RateLimiter(10, TimeSpan.FromSeconds(1));
+
+			limiter.Acquire(9);
+			limiter.Acquire(9).Should().BeGreaterThan(0);
+		}
+
+		[TestMethod]
+		public void ExceedLimit()
+		{
+			var limiter = new RateLimiter(10, TimeSpan.FromSeconds(1));
+
+			Action acquire = () => limiter.Acquire(15);
+			acquire.Should().Throw<ArgumentException>();
+		}
+
+		[TestMethod]
+		public void SuccessAfterWait()
+		{
+			var limiter = new RateLimiter(100, TimeSpan.FromSeconds(1));
+			limiter.Acquire(50);
+
+			// 可能会有点误差，但不应该差的太大
+			var wait = limiter.Acquire(55);
+			wait.Should().BeInRange(49.9, 51);
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(wait));
+			limiter.Acquire(55).Should().BeLessOrEqualTo(0);
+		}
+
+		[TestMethod]
+		public void Smooth()
+		{
+			var limiter = new RateLimiter(50, TimeSpan.FromMilliseconds(100));
+
+			for (int i = 100 ; i > 0; i--)
+			{
+				limiter.Acquire(1).Should().BeLessOrEqualTo(0);
+				Thread.Sleep(2);
+			}
+		}
+
+		[TestMethod]
+		public void Bursty()
+		{
+			var limiter = new RateLimiter(100, TimeSpan.FromSeconds(1));
+
+			for (int i = 100; i > 0; i--)
+			{
+				limiter.Acquire(1).Should().BeLessOrEqualTo(0);
+			}
+
+			limiter.Acquire(1).Should().BeGreaterThan(0);
+		}
+	}
+}
