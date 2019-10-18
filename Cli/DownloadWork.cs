ede00b1a6f0ddba9bd4e4eafe5b44711baedccfb
Kaciras <Kaciras@outlook.com>
2019-10-18 15:47:25 +0800

    Add LocalGalleryStore

--- Cli/DownloadWork.cs
+++ Cli/DownloadWork.cs
@@ -15,7 +15,7 @@ namespace Cli
 	public sealed class DownloadWork
 	{
 		public const int DEFAULT_CONCURRENT = 4;
-		public const int RETRY_TIMES = 3;
+		public const int RETRY_LIMIT = 3;
 
 		public Range Pages { get; set; }
 
@@ -29,8 +29,7 @@ namespace Cli
 		private readonly Gallery gallery;
 		private readonly CancellationTokenSource cancellation;
 
-		private string store;
-		private ISet<string> downloaded;
+		private LocalGalleryStore store;
 
 		private int index;
 		private int endIndex;
@@ -45,22 +44,30 @@ namespace Cli
 
 		public async Task StartDownload()
 		{
-			var directory = DirectoryNameOf(gallery);
-			store = Path.Join(StorePath, directory);
-			Console.WriteLine("本子名：" + directory);
+			store = new LocalGalleryStore(StorePath, gallery);
 
+			var name = gallery.Info.JapaneseName ?? gallery.Info.Name;
+			Console.WriteLine("本子名：" + name);
+
+			// 如果有新版就提示一下，但还是继续下载当前版本
+			var newest = await gallery.GetLatestVersion();
+			if (newest != gallery)
+			{
+				Console.WriteLine($"该本子有新版本：{newest.Uri}");
+			}
+
+			// 如果下载过旧版，就把旧版的图片都迁移过来
 			var old = await GetExistsVersion();
 			if (old != null)
 			{
-				UpdateStoreDirectory(old);
+				old.MigrateTo(store);
+				Console.WriteLine($"已将旧版目录[{old.Name}]合并");
 			}
 			else
 			{
-				Directory.CreateDirectory(store);
+				store.Create();
 			}
 
-
-			downloaded = Force ? new SortedSet<string>() : ScanDownloaded();
 			(index, endIndex) = Pages.GetOffsetAndLength(gallery.Info.Length);
 			endIndex += index;
 
@@ -69,74 +76,14 @@ namespace Cli
 			Console.WriteLine("下载任务结束，共下载了" + downloadSize);
 		}
 
-		private string DirectoryNameOf(Gallery gallery)
-		{
-			var info = gallery.Info;
-			return $"{gallery.Id} - {info.JapaneseName ?? info.Name}";
-		}
-
-		private async Task<Gallery> GetExistsVersion()
+		private async Task<LocalGalleryStore> GetExistsVersion()
 		{
 			for (var v = gallery; v != null; v = await v.GetParent())
 			{
-				var name = DirectoryNameOf(gallery);
-				if (Directory.Exists(Path.Join(StorePath, name)))
-				{
-					return gallery;
-				}
+				var store = new LocalGalleryStore(StorePath, gallery);
+				if (store.Exists()) return store;
 			}
 			return null;
-		}
-
-		private void UpdateStoreDirectory(Gallery oldGallery)
-		{
-			var oldName = DirectoryNameOf(oldGallery);
-			var name = DirectoryNameOf(gallery);
-			Directory.Move(Path.Join(StorePath, oldName), Path.Join(StorePath, name));
-
-			var oldNumberLength = (int)Math.Log10(oldGallery.Info.Length);
-			var numberLength = (int)Math.Log10(gallery.Info.Length);
-			if (numberLength != oldNumberLength)
-			{
-				new DirectoryInfo(store)
-					.EnumerateFiles()
-					.ForEach((file, i) => file.MoveTo(i.ToString().PadLeft(numberLength, '0')));
-			}
-
-			Console.WriteLine($"已将旧版目录合并：{oldName}");
-		}
-
-		/// <summary>
-		/// 扫描存储目录中已存在的图片文件，这些文件将被跳过不再去下载。
-		/// </summary>
-		/// <returns>文件名集合</returns>
-		private ISet<string> ScanDownloaded()
-		{
-			return new DirectoryInfo(store)
-				.EnumerateFiles()
-				.Where(CheckImageFile)
-				.Select(file => file.Name)
-				.ToHashSet();
-		}
-
-		/// <summary>
-		/// 检查图片文件有没有损坏，无法检测出残缺的图片？
-		/// </summary>
-		/// <param name="file">文件对象</param>
-		/// <returns>如果图片正常返回true</returns>
-		public static bool CheckImageFile(FileInfo file)
-		{
-			try
-			{
-				Image.FromFile(file.FullName).Dispose();
-				return true;
-			}
-			catch(OutOfMemoryException)
-			{
-				// 读取失败抛OOM异常什么鬼啦？？？
-				Console.WriteLine($"无法解析的图片文件：{file.Name}");
-				return false;
-			}
 		}
 
 		private async Task RunWorker()
@@ -168,29 +115,24 @@ namespace Cli
 		{
 			cancellation.Token.ThrowIfCancellationRequested();
 
-			var image = await gallery.GetImage(index);
+			var file = store.GetImageFile(index);
 
-			// 使用序号并填充对齐作为文件名，保证文件顺序跟本子里的顺序一致。
-			// 至于图片的原名就没什么用了。
-			var nums = (int)Math.Log10(gallery.Info.Length) + 1;
-			var fileName = index.ToString().PadLeft(nums, '0');
-
-			if (downloaded.Contains(fileName))
+			if (Force && CheckImageFile(file))
 			{
 				return;
 			}
 
-			for (int i = 0; i < RETRY_TIMES; i++)
+			for (int retry = 0; retry < RETRY_LIMIT; retry++)
 			{
 				try
 				{
-					var storePath = Path.Combine(store, fileName);
-					await image.Download(storePath, cancellation.Token);
+					var image = await gallery.GetImage(index);
+					await image.Download(file.FullName, cancellation.Token);
 
 					var time = DateTime.Now.ToLongTimeString();
-					Console.WriteLine($"[{time}] 第{index}张图片{fileName}下载完毕");
+					Console.WriteLine($"[{time}] 第{index}张图片下载完毕");
 
-					downloadSize += new DataSize(new FileInfo(storePath).Length);
+					downloadSize += new DataSize(file.Length);
 					break;
 				}
 				catch (HttpRequestException)
@@ -201,10 +143,30 @@ namespace Cli
 				{
 					// 读取请求体的时候出错。（本地文件错误怎么办？）
 				}
-				Console.WriteLine($"{fileName}下载失败，重试 - {i}");
+				Console.WriteLine($"第{index}张图下载失败，重试 - {retry}");
 			}
 		}
 
 		public void Cancel() => cancellation.Cancel();
+
+		/// <summary>
+		/// 检查图片文件有没有损坏，无法检测出残缺的图片？
+		/// </summary>
+		/// <param name="file">文件对象</param>
+		/// <returns>如果图片正常返回true</returns>
+		public static bool CheckImageFile(FileInfo file)
+		{
+			try
+			{
+				Image.FromFile(file.FullName).Dispose();
+				return true;
+			}
+			catch (OutOfMemoryException)
+			{
+				// 读取失败抛OOM异常什么鬼啦？？？
+				Console.WriteLine($"无法解析的图片文件：{file.Name}");
+				return false;
+			}
+		}
 	}
 }
---
+++ Cli/LocalGalleryStore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Core;
+
+namespace Cli
+{
+	public sealed class LocalGalleryStore
+	{
+		public string Name { get; }
+
+		readonly Gallery gallery;
+		readonly string directory;
+
+		public LocalGalleryStore(string repository, Gallery gallery)
+		{
+			this.gallery = gallery;
+
+			// 因为本子名可以重名，所以得把ID带在目录名里
+			var info = gallery.Info;
+			Name = $"{gallery.Id} - {info.JapaneseName ?? info.Name}";
+			directory = Path.Join(repository, Name);
+		}
+
+		// 使用序号并填充对齐作为文件名，保证文件顺序跟本子里的顺序一致。
+		// 至于图片的原名就没什么用了。
+		public FileInfo GetImageFile(int index)
+		{
+			var nums = (int)Math.Log10(gallery.Info.Length) + 1;
+			var fileName = index.ToString().PadLeft(nums, '0');
+			return new FileInfo(Path.Join(directory, fileName));
+		}
+
+		public void MigrateTo(LocalGalleryStore target)
+		{
+			Directory.Move(directory, target.directory);
+
+			var nums = (int)Math.Log10(target.gallery.Info.Length) + 1;
+			var oldNums = (int)Math.Log10(gallery.Info.Length) + 1;
+
+			if (oldNums != nums)
+			{
+				new DirectoryInfo(directory)
+					.EnumerateFiles()
+					.ForEach((file, i) => file.MoveTo(target.GetImageFile(i).FullName));
+			}
+		}
+
+		public bool Exists() => Directory.Exists(directory);
+
+		public void Create() => Directory.CreateDirectory(directory);
+	}
+}
