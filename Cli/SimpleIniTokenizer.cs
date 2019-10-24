d7174eca8235ec0873da39fb072305a9e29498c9
Kaciras <Kaciras@outlook.com>
2019-10-24 20:26:47 +0800

    Intro SimpleIniTokenizer

---
+++ Cli/SimpleIniTokenizer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+// 仿照 Utf8JsonReader 写一个INI文件解析器，学习一下Span的用法。
+namespace Cli
+{
+	public enum IniToken
+	{
+		None,
+		Comment,
+		Section,
+		Key,
+		Value,
+	}
+
+	// 不支持行内注释，等号和空格都是分隔符
+	public ref struct SimpleIniTokenizer
+	{
+		public IniToken TokenType { get; private set; }
+		public ReadOnlySpan<char> CurrentValue { get; private set; }
+
+		private readonly ReadOnlySpan<char> buffer;
+
+		private int consumed;
+
+		public SimpleIniTokenizer(ReadOnlySpan<char> buffer)
+		{
+			this.buffer = buffer;
+			consumed = 0;
+			TokenType = IniToken.None;
+			CurrentValue = ReadOnlySpan<char>.Empty;
+		}
+
+		public bool Read()
+		{
+			if(consumed >= buffer.Length)
+			{
+				return false;
+			}
+
+			switch (buffer[consumed])
+			{
+				case '#':
+				case ';':
+					ConsumeComment();
+					TokenType = IniToken.Comment;
+					break;
+				case '[':
+					ConsumeSection();
+					TokenType = IniToken.Section;
+					break;
+				case '\r':
+				case '\n':
+				case '\t':
+				case ' ':
+					SkipWhiteSpace();
+					return Read();
+				case '=':
+					ConsumeValue();
+					TokenType = IniToken.Value;
+					break;
+				default:
+					ConsumeKey();
+					TokenType = IniToken.Key;
+					break;
+			}
+
+			return true;
+		}
+
+		public void SkipWhiteSpace()
+		{
+			// Create local copy to avoid bounds checks.
+			var local = buffer;
+			for (; consumed < local.Length; consumed++)
+			{
+				switch (local[consumed])
+				{
+					case '\r':
+					case '\n':
+					case '\t':
+					case ' ':
+						break;
+					default:
+						return;
+				}
+			}
+		}
+
+		public void ConsumeComment()
+		{
+			var local = buffer.Slice(consumed + 1);
+			var j = 0;
+
+			for (; j < local.Length; j++)
+			{
+				switch (local[j])
+				{
+					case '\r':
+					case '\n':
+						goto SearchEnd;
+				}
+			}
+		SearchEnd:
+			consumed += j + 1;
+			CurrentValue = local.Slice(0, j);
+		}
+
+		public void ConsumeSection()
+		{
+			var local = buffer.Slice(consumed + 1);
+			var j = local.IndexOf(']');
+			if (j < 0)
+			{
+				throw new Exception("数据不完整");
+			}
+			consumed += j + 2;
+			CurrentValue = local.Slice(0, j);
+		}
+
+		public void ConsumeKey()
+		{
+			var local = buffer.Slice(consumed);
+			var j = 0;
+
+			for (; j < local.Length; j++)
+			{
+				switch (local[j])
+				{
+					case '\r':
+					case '\n':
+					case '\t':
+					case ' ':
+					case '=':
+						goto SearchEnd;
+				}
+			}
+		SearchEnd:
+			consumed += j;
+			CurrentValue = local.Slice(0, j);
+		}
+
+		public void ConsumeValue()
+		{
+			if (TokenType != IniToken.Key)
+			{
+				throw new Exception("等号出现在一行的开头");
+			}
+
+			// 前面的等号没有跳过
+			consumed++;
+			SkipWhiteSpace();
+
+			var local = buffer.Slice(consumed);
+			var j = 0;
+
+			for (; j < local.Length; j++)
+			{
+				switch (local[j])
+				{
+					case '\r':
+					case '\n':
+						goto SearchEnd;
+				}
+			}
+		SearchEnd:
+			consumed += j;
+			CurrentValue = local.Slice(0, j);
+		}
+	}
+}
---
+++ Test/SimpleIniTokenizerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Cli;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+	[TestClass]
+	public class SimpleIniTokenizerTest
+	{
+		private void AssertToken(ref SimpleIniTokenizer tokenizer, IniToken type, string value)
+		{
+			Assert.IsTrue(tokenizer.Read());
+			Assert.AreEqual(type, tokenizer.TokenType);
+			Assert.AreEqual(value, new string(tokenizer.CurrentValue));
+		}
+
+		[TestMethod]
+		public void MyTestMethod()
+		{
+			var buffer = File.ReadAllText(@"WebArchive/IniTokenizerTest.ini");
+			var instance = new SimpleIniTokenizer(buffer);
+
+			AssertToken(ref instance, IniToken.Comment, " 用于测试 SimpleIniTokenizer 的样例文件");
+			AssertToken(ref instance, IniToken.Comment, " This file is a simple for test SimpleIniTokenizer");
+						 
+			AssertToken(ref instance, IniToken.Section, "Kaciras");
+			AssertToken(ref instance, IniToken.Key, "Looks");
+			AssertToken(ref instance, IniToken.Value, "Handsome");
+			AssertToken(ref instance, IniToken.Key, "key");
+			AssertToken(ref instance, IniToken.Value, @"value\no\escape");
+
+			AssertToken(ref instance, IniToken.Section, "SimpleIniTokenizer");
+			AssertToken(ref instance, IniToken.Comment, " comment before option");
+			AssertToken(ref instance, IniToken.Key, "Option.Alias");
+			AssertToken(ref instance, IniToken.Value, "Property");
+			AssertToken(ref instance, IniToken.Key, "Enable-Fast-preasing");
+
+			Assert.IsFalse(instance.Read());
+		}
+	}
+}
